7eaa55a725ca1b76ea3d7c40f336d9d63b35cb5f
Jan Vlcek <vlki@vlki.cz>
2011-05-02 15:12:19 +0200

    Running tests from user-defined directory

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -17,6 +17,11 @@ TESTS_MODULE_DIR=$( dirname $0 )
 BEAKERLIB_TESTS_DIR="$TESTS_MODULE_DIR/RHNSatellite"
 ANALYZE_SCRIPT="$TESTS_MODULE_DIR/analyze_beakerlib_journal.py"
 
+# if first parameter is specified, use it as tests directory
+if [ -n "$1" ]; then
+  BEAKERLIB_TESTS_DIR="$1"
+fi
+
 stdoutTempFile=$( mktemp )
 
 all=0
@@ -25,6 +30,13 @@ pass=0
 for makefilePath in $( find $BEAKERLIB_TESTS_DIR -type f -name Makefile )
 do
   testDirPath=$( dirname $makefilePath )
+
+  # Execute that make only if there are files PURPOSE and runtest.sh
+  if [ ! \( -f "$testDirPath/PURPOSE" -a -f "$testDirPath/runtest.sh" \) ]
+  then
+    continue
+  fi
+
   make -C "$testDirPath" > "$stdoutTempFile"
  
   # Parse the path to a XML journal from the output
@@ -50,7 +62,11 @@ done
 # Remove the temporary file used for test outputs
 rm -f "$stdoutTempFile"
 
-echo "Passed $pass out of $all tests"
+if [ $all -eq 0 ]; then
+  echo "No tests found in $BEAKERLIB_TESTS_DIR"
+else
+  echo "Passed $pass out of $all tests"
+fi
 
 if [ $pass -eq $all ]; then
   exit 0
